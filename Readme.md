a6b52b63de9090f9fa203e7ca4a57293b13efbdb
Julio Cid <59150903+Julius-cell@users.noreply.github.com>
2021-06-08 23:34:31 -0400

    First commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+
+
+### TODOS: 
+1. Modificar el manejo de errores (errorController.js) a las necesidades de la app
+2. Implement your routes in the Routes folder
+3. Paste you Public Files into the Public Folder
---
+++ controllers/authController.js
@@ -0,0 +1,57 @@
+const { googleVerify } = require('../utils/googleVerify');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/appError');
+const User = require('../models/userModel');
+const { createSendToken } = require('../utils/jwt');
+
+const sendResponse = (data, statusCode, req, res) => {
+  res.status(statusCode).json({
+    status: 'success',
+    data
+  });
+};
+
+// TODO: Modificar el amnejo de errores (errorController) a las necesidades de la app
+exports.googleSignIn = async (req, res, next) => {
+  const googleToken = req.body.token;
+  try {
+    const { name, email, picture } = await googleVerify(googleToken);
+    console.log(name, email, picture);
+    const userDB = await User.findOne({email});
+    let user;
+    if (!userDB) {
+      user = new User({
+        name,
+        email,
+        photo: picture,
+        google: true,
+        password: '@@@@@@@@',
+        passwordConfirm: '@@@@@@@@'
+      })
+    } else {
+      user = useDB;
+      user.google = true;
+    }
+    // Save in DB
+    await user.save();
+    // Generate JWT - TOKEN
+    console.log(userDB);
+    createSendToken(userDB, 200, req, res);
+    
+    // const data = {
+    //   message: 'Google SignIn',
+    //   user: {
+    //     name,
+    //     email,
+    //     picture
+    //   }
+    // }
+    // sendResponse(data, 200, req, res);
+  } catch (error) {
+    const data = {
+      message: error.name,
+      error: error.message
+    }
+    sendResponse(data, 401, req, res);
+  }
+};
